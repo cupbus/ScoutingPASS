1eca78c8a21f1ec9185bb985e518cad97f92f494
cupbus <99226336+cupbus@users.noreply.github.com>
2023-03-19 23:58:20 -0400

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "P7MwA8hQ1tZpssbKqhg7Re9PBzJYVwB15zDmy05G4LK6XvuA0eq9LtOxEeAW6m3P";
 /**
  * Get list of teams in event
  *
